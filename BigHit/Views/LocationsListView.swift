c4e7038e0d50c8066eeec3a66215c00424454969
Jason Salazar <103016083+Jasonsal23@users.noreply.github.com>
2024-02-07 14:09:45 -0800

    added new shop in kenosha

---
+++ BigHit/Views/LocationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsListView.swift
+//  BigHit
+//
+//  Created by Jason on 2/6/24.
+//
+
+import SwiftUI
+
+struct Locations_ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Locations_ListView()
+}
